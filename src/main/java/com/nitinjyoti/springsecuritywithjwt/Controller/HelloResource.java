8e2a359557a6614b12f804aa2dcceea91d79f568
Darshan Parekh <parekhdarshan6@gmail.com>
2022-06-05 00:24:56 +0530

    changed to rest controller

--- src/main/java/com/nitinjyoti/springsecuritywithjwt/Controller/HelloResource.java
+++ src/main/java/com/nitinjyoti/springsecuritywithjwt/Controller/HelloResource.java
@@ -1,9 +1,10 @@
-package com.nitinjyoti.springsecuritywithjwt;
+package com.nitinjyoti.springsecuritywithjwt.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HelloResource {
 
     @RequestMapping({"/hello"})
